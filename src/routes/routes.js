1f2413a1acd0de2fc221d008e031af4bd8f530d5
anastasiya.melnichenko <anastasiya.melnichenko@ramax.ru>
2020-06-06 16:37:57 +0300

    Updates

--- src/routes/routes.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Main } from '../pages/main';
-import { Pizzas } from '../pages/pizzas';
-
-const Routes = {
-  '/': () => <Main />,
-  '/pizzas': () => <Pizzas />,
-};
-export default Routes;
